0d3c50d183e2083aca96c348346dc85ac589cc14
Rostik Novosad <rostiknovosad4002@gmail.com>
2025-01-18 19:18:54 +0200

    start create footer component

--- components/general/BFooter.vue
+++ components/general/BFooter.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Footer component</div>
+  <div class="flex justify-between">
+    <div class="flex flex-col gap-10">
+      <img src="~/assets/images/logo.png" width="97" height="90" alt="logo image" />
+      <ul class="flex gap-10">
+        <li>About</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Gallery</li>
+        <li>Team</li>
+      </ul>
+    </div>
+  </div>
 </template>
