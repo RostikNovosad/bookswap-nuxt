02be8eb517d6590c8d9600860c3323350912acd7
Rostik Novosad <rostiknovosad4002@gmail.com>
2025-01-21 23:04:56 +0200

    remove scroll if mobile menu open

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,15 @@
   </main>
   <BFooter />
 </template>
+<script setup lang="ts">
+import { watch } from "vue";
+import { useBurgerMenuStore } from '@/stores/burgerMenu';
+const store = useBurgerMenuStore();
+watch(() => store.burgerOpen, (newValue) => {
+  if (newValue) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
+</script>
