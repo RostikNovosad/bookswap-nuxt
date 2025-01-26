76afcc1210938c5cf1caa32aec00387d43157bc9
Rostik Novosad <rostiknovosad4002@gmail.com>
2025-01-26 17:27:57 +0200

    minor fixes

--- components/layout/BLastAdded.vue
+++ components/layout/BLastAdded.vue
@@ -1,86 +1,12 @@
 <script setup lang="ts">
-import {onMounted} from "vue";
-import { useBooksList } from "@/stores/booksList";
-const store = useBooksList();
-const books = store.books;
-onMounted(() => {
-  store.fetchBooks(); // Завантаження даних при монтуванні
-});
 
-
-const containerRef = ref(null)
-const swiper = useSwiper(containerRef, {
-  effect: 'creative',
-  loop: true,
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false, // Запобігає зупинці autoplay при взаємодії
-
-  },
-  breakpoints: {
-    1440: {
-      spaceBetween: 40,
-      slidesPerView: 2.6,
-
-    },
-    1280: {
-      spaceBetween: 40,
-      slidesPerView: 2.15,
-
-    },
-    1024: {
-      spaceBetween: 20,
-      slidesPerView: 2.1,
-    },
-    768: {
-      spaceBetween: 20,
-      slidesPerView: 2,
-    },
-    320: {
-      spaceBetween: 20,
-      slidesPerView: 1,
-    },
-  },
-  creativeEffect: {
-    prev: {
-      shadow: true,
-      translate: [0, 0, -400],
-    },
-    next: {
-      shadow: true,
-      translate: [0, 0, -400],
-    },
-  },
-})
 </script>
 
 <template>
   <section class="pb-10 lg:pb-20">
     <div class="flex justify-between items-end mb-5">
       <BSectionTitle>Останні додані</BSectionTitle>
-
-      <div class="flex gap-4 ">
-        <button @click="swiper.prev()" class="">
-          <img src="~/assets/images/prev.svg" alt="">
-        </button>
-
-        <button @click="swiper.next()" class="">
-          <img src="~/assets/images/next.svg" alt="">
-        </button>
-      </div>
     </div>
-
-    <ClientOnly>
-    <swiper-container ref="containerRef" class="flex gap-10 overflow-x-hidden">
-      <swiper-slide
-          v-for="(book, idx) in books"
-          :key="idx"
-          class="pb-10"
-      >
-        <BBookCard  :key="book.id" :id="book.id" :imageUrl="book.imageUrl" :title="book.title" :description="book.description" :condition="book.condition" cardDirection="row" />
-      </swiper-slide>
-    </swiper-container>
-  </ClientOnly>
-
+    <BLastAddedSwiper />
   </section>
 </template>
---
+++ components/layout/BLastAddedSwiper.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+import { useBooksList } from "@/stores/booksList";
+const store = useBooksList();
+const books = store.books;
+onMounted(() => {
+  store.fetchBooks(); // Завантаження даних при монтуванні
+});
+
+
+const containerRef = ref(null)
+const swiper = useSwiper(containerRef, {
+  effect: 'creative',
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false, // Запобігає зупинці autoplay при взаємодії
+
+  },
+  breakpoints: {
+    1440: {
+      spaceBetween: 40,
+      slidesPerView: 2.6,
+
+    },
+    1280: {
+      spaceBetween: 40,
+      slidesPerView: 2.15,
+
+    },
+    1024: {
+      spaceBetween: 20,
+      slidesPerView: 2.1,
+    },
+    768: {
+      spaceBetween: 20,
+      slidesPerView: 2,
+    },
+    320: {
+      spaceBetween: 20,
+      slidesPerView: 1,
+    },
+  },
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+  },
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <swiper-container ref="containerRef" class="flex gap-10 overflow-x-hidden">
+      <swiper-slide
+          v-for="(book, idx) in books"
+          :key="idx"
+          class="pb-10"
+      >
+        <BBookCard  :key="book.id" :id="book.id" :imageUrl="book.imageUrl" :title="book.title" :description="book.description" :condition="book.condition" cardDirection="row" />
+      </swiper-slide>
+    </swiper-container>
+  </ClientOnly>
+  <div class="flex gap-4 ">
+    <button @click="swiper.prev()" class="">
+      <img src="~/assets/images/prev.svg" alt="">
+    </button>
+
+    <button @click="swiper.next()" class="">
+      <img src="~/assets/images/next.svg" alt="">
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
